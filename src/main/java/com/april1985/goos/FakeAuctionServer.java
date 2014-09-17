5f5544d7a134da153ab64bcb7d95d210e2038e0f
derekhe <derekhe@april1985.com>
2014-09-17 21:14:51 +1000

    First end to end test.

---
+++ src/main/java/com/april1985/goos/FakeAuctionServer.java
@@ -0,0 +1,25 @@
+package com.april1985.goos;
+
+/**
+ * Created by sche on 9/17/14.
+ */
+public class FakeAuctionServer {
+    public FakeAuctionServer(String id) {
+
+    }
+
+    public void startSellingItem() {
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+
+    }
+
+    public void announceClosed() {
+
+    }
+
+    public void stop() {
+
+    }
+}
