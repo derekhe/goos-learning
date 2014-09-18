4cc1fc48a31b528c2c77074786934f0a514aad97
derekhe <derekhe@april1985.com>
2014-09-18 22:46:27 +1000

    P124  Pass auction sniper tests.

--- src/main/java/com/april1985/goos/AuctionSniper.java
+++ src/main/java/com/april1985/goos/AuctionSniper.java
@@ -4,10 +4,13 @@ package com.april1985.goos;
  * Created by sche on 9/18/14.
  */
 public class AuctionSniper {
+    private SniperListener sniperListener;
+
     public AuctionSniper(SniperListener sniperListener) {
+        this.sniperListener = sniperListener;
     }
 
     public void auctionClosed() {
-
+        sniperListener.sniperLost();
     }
 }
