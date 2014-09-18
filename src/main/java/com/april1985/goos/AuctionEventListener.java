179a38f00dd981584e973767c0acd7dc541ecf89
derekhe <derekhe@april1985.com>
2014-09-18 22:27:51 +1000

    P119 Write a second test for bidding

--- src/main/java/com/april1985/goos/AuctionEventListener.java
+++ src/main/java/com/april1985/goos/AuctionEventListener.java
@@ -4,5 +4,6 @@ package com.april1985.goos;
  * Created by sche on 9/18/14.
  */
 public interface AuctionEventListener {
-    public void auctionClosed();
+    void auctionClosed();
+    void currentPrice(int price, int increment);
 }
--- src/main/java/com/april1985/goos/Main.java
+++ src/main/java/com/april1985/goos/Main.java
@@ -84,4 +84,9 @@ public class Main implements AuctionEventListener {
             }
         });
     }
+
+    @Override
+    public void currentPrice(int price, int increment) {
+
+    }
 }
