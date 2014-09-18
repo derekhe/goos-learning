96487527523f0af5c152a9dcbb4faaf241b573f7
derekhe <derekhe@april1985.com>
2014-09-18 22:12:05 +1000

    P116 Add unit tests for AuctionMessageTranslator

---
+++ src/main/java/com/april1985/goos/AuctionMessageTranslator.java
@@ -0,0 +1,13 @@
+package com.april1985.goos;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
+
+/**
+ * Created by sche on 9/18/14.
+ */
+public class AuctionMessageTranslator {
+    public void processMessage(Chat chat, Message message) {
+
+    }
+}
